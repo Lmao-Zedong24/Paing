d9eeead5a2e360dcf35697575bbd0bb0fb6a5a15
Vadim <vboldireff01@gmail.com>
2023-11-13 13:27:21 -0500

    Setup VR

--- Source/Paing/Paing.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Paing.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Paing, "Paing" );
