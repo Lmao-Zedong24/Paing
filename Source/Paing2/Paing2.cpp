b118defba1c0079bedc9d8fdba14297b79fdd904
Vadim <vboldireff01@gmail.com>
2023-11-14 11:25:58 -0500

    Debug C++ setup

---
+++ Source/Paing2/Paing2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Paing2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Paing2, "Paing2" );
